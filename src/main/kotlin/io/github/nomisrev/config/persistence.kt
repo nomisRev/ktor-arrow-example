2aed329b9ba8d7b0cc7f91845417f649053c1fec
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-04-15 18:29:37 +0200

    Use value classes for ids in SqlDelight

--- src/main/kotlin/io/github/nomisrev/config/persistence.kt
+++ src/main/kotlin/io/github/nomisrev/config/persistence.kt
@@ -7,9 +7,13 @@ import arrow.fx.coroutines.continuations.resource
 import arrow.fx.coroutines.fromCloseable
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.github.nomisrev.repo.ArticleId
+import io.github.nomisrev.repo.UserId
 import io.github.nomisrev.sqldelight.Articles
 import io.github.nomisrev.sqldelight.Comments
 import io.github.nomisrev.sqldelight.SqlDelight
+import io.github.nomisrev.sqldelight.Tags
+import io.github.nomisrev.sqldelight.Users
 import java.time.OffsetDateTime
 import javax.sql.DataSource
 
@@ -30,12 +34,22 @@ fun sqlDelight(dataSource: DataSource): Resource<SqlDelight> = resource {
   SqlDelight.Schema.create(driver)
   SqlDelight(
     driver,
-    Articles.Adapter(OffsetDateTimeAdapter, OffsetDateTimeAdapter),
-    Comments.Adapter(OffsetDateTimeAdapter, OffsetDateTimeAdapter)
+    Articles.Adapter(articleIdAdapter, userIdAdapter, offsetDateTimeAdapter, offsetDateTimeAdapter),
+    Comments.Adapter(offsetDateTimeAdapter, offsetDateTimeAdapter),
+    Tags.Adapter(articleIdAdapter),
+    Users.Adapter(userIdAdapter)
   )
 }
 
-private object OffsetDateTimeAdapter : ColumnAdapter<OffsetDateTime, String> {
-  override fun decode(databaseValue: String): OffsetDateTime = OffsetDateTime.parse(databaseValue)
-  override fun encode(value: OffsetDateTime): String = value.toString()
-}
+private val articleIdAdapter = columnAdapter(::ArticleId, ArticleId::serial)
+private val userIdAdapter = columnAdapter(::UserId, UserId::serial)
+private val offsetDateTimeAdapter = columnAdapter(OffsetDateTime::parse, OffsetDateTime::toString)
+
+private inline fun <A : Any, B> columnAdapter(
+  crossinline decode: (databaseValue: B) -> A,
+  crossinline encode: (value: A) -> B
+): ColumnAdapter<A, B> =
+  object : ColumnAdapter<A, B> {
+    override fun decode(databaseValue: B): A = decode(databaseValue)
+    override fun encode(value: A): B = encode(value)
+  }
