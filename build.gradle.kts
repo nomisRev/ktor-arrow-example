606bd2ae9771c6c52997a6d9e41b3b3b9e55b7b2
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-06-18 15:16:20 +0200

    Remove Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
+import org.jetbrains.kotlin.config.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 @Suppress("DSL_SCOPE_VIOLATION") plugins {
   application
   alias(libs.plugins.kotest.multiplatform)
   id(libs.plugins.kotlin.jvm.pluginId)
-  alias(libs.plugins.arrowGradleConfig.formatter)
   alias(libs.plugins.dokka)
   id(libs.plugins.detekt.pluginId)
   alias(libs.plugins.kover)
@@ -19,7 +19,7 @@ application {
 sqldelight {
   database("SqlDelight") {
     packageName = "io.github.nomisrev.sqldelight"
-    dialect = libs.sqldelight.postgresql.asString()
+    dialect(libs.sqldelight.postgresql.asString())
   }
 }
 
@@ -35,11 +35,9 @@ repositories {
 tasks {
   withType<KotlinCompile>().configureEach {
     kotlinOptions {
-      jvmTarget = "1.8"
+      jvmTarget = "${JavaVersion.VERSION_11}"
       freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers"
     }
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
   }
 
   test {
