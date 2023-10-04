560f382c36c5785f52377ce4998251929fea7456
arios <ariostonj@hotmail.com>
2023-10-03 23:52:06 -0300

    tagRoute tests added

---
+++ src/test/kotlin/io/github/nomisrev/routes/TagRouteSpec.kt
@@ -0,0 +1,24 @@
+package io.github.nomisrev.routes
+
+import io.github.nomisrev.withServer
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
+
+
+class TagRouteSpec :
+  StringSpec({
+    "can get all tags" {
+      withServer {
+        val response = get("/tags") { contentType(ContentType.Application.Json) }
+
+        response.status shouldBe HttpStatusCode.OK
+        response.body<TagResponse>().tags shouldHaveSize 0
+      }
+    }
+  })
