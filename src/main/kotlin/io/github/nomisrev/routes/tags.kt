5c19e3104f4d3db7a98a5c423e6bffed44f13f71
arios <ariostonj@hotmail.com>
2023-10-03 23:50:32 -0300

    tagRoutes created

---
+++ src/main/kotlin/io/github/nomisrev/routes/tags.kt
@@ -0,0 +1,24 @@
+@file:Suppress("MatchingDeclarationName")
+
+package io.github.nomisrev.routes
+
+import io.github.nomisrev.service.TagService
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
+import kotlinx.serialization.Serializable
+
+@Serializable data class TagResponse(val tags: List<String>)
+
+fun Application.tagRoutes(tagService: TagService) = routing {
+  route("/tags") {
+    /* Registration: GET /api/tags */
+    get {
+      val tags = tagService.selectTags()
+      call.respond(TagResponse(tags))
+    }
+  }
+}
