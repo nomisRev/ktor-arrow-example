76bea8b64edebda3dd37fdf1973bc33f0b12a642
marinjuricev <mjuric00@fesb.hr>
2023-10-31 17:53:16 +0100

    Remove the `parameter` implementation, we have access to the route object inside the type safe API

--- src/main/kotlin/io/github/nomisrev/routes/profile.kt
+++ src/main/kotlin/io/github/nomisrev/routes/profile.kt
@@ -1,67 +1,51 @@
+@file:Suppress("MatchingDeclarationName")
+
 package io.github.nomisrev.routes
 
-import arrow.core.Either
-import arrow.core.left
 import arrow.core.raise.either
-import arrow.core.right
-import io.github.nomisrev.DomainError
-import io.github.nomisrev.MissingParameter
 import io.github.nomisrev.auth.jwtAuth
 import io.github.nomisrev.repo.UserPersistence
 import io.github.nomisrev.service.JwtService
-import io.github.nomisrev.IncorrectJson
-import io.github.nomisrev.repo.UserPersistence
 import io.ktor.http.HttpStatusCode
 import io.ktor.resources.Resource
 import io.ktor.server.resources.delete
-import io.ktor.server.routing.Route
-import io.ktor.server.application.ApplicationCall
-import io.ktor.server.application.call
 import io.ktor.server.resources.get
 import io.ktor.server.routing.Route
-import io.ktor.util.pipeline.PipelineContext
 import kotlinx.serialization.Serializable
 
-@Serializable
-data class ProfileWrapper<T : Any>(val profile: T)
+@Serializable data class ProfileWrapper<T : Any>(val profile: T)
 
 @Serializable
 data class Profile(
-    val username: String,
-    val bio: String,
-    val image: String,
-    val following: Boolean
+  val username: String,
+  val bio: String,
+  val image: String,
+  val following: Boolean
 )
 
 @Resource("/profiles")
 data class ProfilesResource(val parent: RootResource = RootResource) {
-    @Resource("/{username}/follow")
-    data class Follow(val parent: ProfilesResource = ProfilesResource(), val username: String)
+  @Resource("/{username}")
+  data class Username(val parent: ProfilesResource = ProfilesResource(), val username: String)
 
-    @Resource("/{$USERNAME}")
-    data class Username(val parent: ProfileResource = ProfileResource(), val username: String)
+  @Resource("/{username}/follow")
+  data class Follow(val parent: ProfilesResource = ProfilesResource(), val username: String)
 }
 
-fun Route.profileRoutes(
-    userPersistence: UserPersistence,
-    jwtService: JwtService
-) {
-  get<ProfileResource.Username> { route ->
-    either { repo.selectProfile(route.username).bind() }.respond(HttpStatusCode.OK)
-  }delete<ProfilesResource.Follow> { follow ->
-        jwtAuth(jwtService) { (_, userId) ->
-            either {
-                userPersistence.unfollowProfile(follow.username, userId)
-                val userUnfollowed = userPersistence.select(follow.username).bind()
-                ProfileWrapper(Profile(userUnfollowed.username, userUnfollowed.bio, userUnfollowed.image, false))
-            }.respond(HttpStatusCode.OK)
+fun Route.profileRoutes(userPersistence: UserPersistence, jwtService: JwtService) {
+  get<ProfilesResource.Username> { route ->
+    either { userPersistence.selectProfile(route.username).bind() }.respond(HttpStatusCode.OK)
+  }
+  delete<ProfilesResource.Follow> { follow ->
+    jwtAuth(jwtService) { (_, userId) ->
+      either {
+          userPersistence.unfollowProfile(follow.username, userId)
+          val userUnfollowed = userPersistence.select(follow.username).bind()
+          ProfileWrapper(
+            Profile(userUnfollowed.username, userUnfollowed.bio, userUnfollowed.image, false)
+          )
         }
+        .respond(HttpStatusCode.OK)
     }
+  }
 }
-
-private inline fun <reified A : Any> PipelineContext<Unit, ApplicationCall>.parameter(
-    parameter: String,
-    noinline toRight: (String) -> A,
-): Either<DomainError, A> =
-    call.parameters[parameter]?.let(toRight)?.right() ?: MissingParameter(parameter).left()
-
