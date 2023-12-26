d51604d4842ad1ef8016e4bf7e3e2a175a2e5a55
arios <ariostonj@hotmail.com>
2023-12-25 21:35:28 -0300

    test for article comments for slug

--- src/test/kotlin/io/github/nomisrev/routes/ArticlesRouteSpec.kt
+++ src/test/kotlin/io/github/nomisrev/routes/ArticlesRouteSpec.kt
@@ -1,18 +1,23 @@
 package io.github.nomisrev.routes
 
-import arrow.core.flatMap
-import io.github.nefilim.kjwt.JWSHMAC512Algorithm
-import io.github.nefilim.kjwt.JWT
+import io.github.nomisrev.KotestProject
+import io.github.nomisrev.auth.JwtToken
 import io.github.nomisrev.repo.UserId
 import io.github.nomisrev.service.CreateArticle
+import io.github.nomisrev.service.Login
 import io.github.nomisrev.service.RegisterUser
 import io.github.nomisrev.withServer
 import io.kotest.assertions.arrow.core.shouldBeRight
-import io.kotest.assertions.arrow.core.shouldBeSome
 import io.kotest.core.spec.style.StringSpec
 import io.ktor.client.call.body
 import io.ktor.client.plugins.resources.get
+import io.ktor.client.plugins.resources.post
+import io.ktor.client.request.bearerAuth
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
+import kotlin.properties.Delegates
 
 class ArticlesRouteSpec :
   StringSpec({
@@ -25,6 +30,28 @@ class ArticlesRouteSpec :
     val validTitle = "Fake Article Arrow "
     val validDescription = "This is a fake article description."
     val validBody = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
+    var token: JwtToken by Delegates.notNull()
+    var userId: UserId by Delegates.notNull()
+
+    beforeAny {
+      KotestProject.dependencies
+        .get()
+        .userService
+        .register(RegisterUser(validUsername, validEmail, validPw))
+        .shouldBeRight()
+    }
+
+    beforeTest {
+      token =
+        KotestProject.dependencies
+          .get()
+          .userService
+          .login(Login(validEmail, validPw))
+          .shouldBeRight()
+          .first
+      userId = KotestProject.dependencies.get().jwtService.verifyJwtToken(token).shouldBeRight()
+    }
 
     "Article by slug not found" {
       withServer {
@@ -39,17 +66,10 @@ class ArticlesRouteSpec :
 
     "Can get an article by slug" {
       withServer { dependencies ->
-        val userId =
-          dependencies.userService
-            .register(RegisterUser(validUsername, validEmail, validPw))
-            .flatMap { JWT.decodeT(it.value, JWSHMAC512Algorithm) }
-            .map { it.claimValueAsLong("id").shouldBeSome() }
-            .shouldBeRight()
-
         val article =
           dependencies.articleService
             .createArticle(
-              CreateArticle(UserId(userId), validTitle, validDescription, validBody, validTags)
+              CreateArticle(userId, validTitle, validDescription, validBody, validTags)
             )
             .shouldBeRight()
 
@@ -57,6 +77,73 @@ class ArticlesRouteSpec :
 
         assert(response.status == HttpStatusCode.OK)
         assert(response.body<SingleArticleResponse>().article == article)
+      }
+    }
+
+    "Can add a comment to an article" {
+      withServer { dependencies ->
+        val comment = "This is a comment"
+        val article =
+          dependencies.articleService
+            .createArticle(
+              CreateArticle(userId, validTitle, validDescription, validBody, validTags)
+            )
+            .shouldBeRight()
+
+        val response =
+          post(ArticlesResource.Comments(slug = article.slug)) {
+            contentType(ContentType.Application.Json)
+            bearerAuth(token.value)
+            setBody(NewComment(comment))
+          }
+
+        assert(response.status == HttpStatusCode.OK)
+        with(response.body<SingleCommentResponse>()) {
+          assert(this.comment.body == comment)
+          assert(this.comment.author.username == validUsername)
+        }
+      }
+    }
+
+    "Can not add a comment to an article with invalid token" {
+      withServer { dependencies ->
+        val comment = "This is a comment"
+        val article =
+          dependencies.articleService
+            .createArticle(
+              CreateArticle(userId, validTitle, validDescription, validBody, validTags)
+            )
+            .shouldBeRight()
+
+        val response =
+          post(ArticlesResource.Comments(slug = article.slug)) {
+            contentType(ContentType.Application.Json)
+            bearerAuth("invalid token")
+            setBody(NewComment(comment))
+          }
+
+        assert(response.status == HttpStatusCode.Unauthorized)
+      }
+    }
+
+    "Can not add a comment to an article with empty body" {
+      withServer { dependencies ->
+        val comment = ""
+        val article =
+          dependencies.articleService
+            .createArticle(
+              CreateArticle(userId, validTitle, validDescription, validBody, validTags)
+            )
+            .shouldBeRight()
+
+        val response =
+          post(ArticlesResource.Comments(slug = article.slug)) {
+            contentType(ContentType.Application.Json)
+            bearerAuth(token.value)
+            setBody(NewComment(comment))
+          }
+
+        assert(response.status == HttpStatusCode.UnprocessableEntity)
       }
     }
   })
